629982a6a9d1f00071eb12620defe37b2f3dae6a
Asım <asimzorlu@gmail.com>
2022-10-28 16:53:38 +0200

    Update get_credit_on_YouLikeHits.py

--- get_credit_on_YouLikeHits.py
+++ get_credit_on_YouLikeHits.py
@@ -2,11 +2,8 @@ from time import sleep
 import sys
 sys.path.append(r'C:\\Projects\\Get_Free_Followers')
 from init import *
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 
-class Instagram:
+class YouLikeHits:
     def setup(self):
         Setup.init(self)
 
@@ -15,15 +12,15 @@ class Instagram:
         self.browser.get('https://www.youlikehits.com/login.php')
         sleep(10)
         uid = self.browser.find_element(By.ID, 'username')
-        uid.send_keys(YOUR YOULIKEHITS USERNAME)  
+        uid.send_keys('YOUR YOULIKEHITS USERNAME')
         sleep(2)
         pwd = self.browser.find_element(By.ID, 'password')
-        pwd.send_keys(YOUR YOULIKEHITS PASSWORD)
+        pwd.send_keys('YOUR YOULIKEHITS PASSWORD')
         sleep(2)
         btn = self.browser.find_element(By.XPATH, '/html/body/table[2]/tbody/tr/td/table/tbody/tr/td/table/tbody/tr[2]/td/center/form/table/tbody/tr[3]/td/span/input')
         btn.click()
         sleep(2)
-        #You must add your social media account to YouLikeHits system.
+
         print('Getting credits... Please do not terminate the program.')
         while (True):
             self.browser.get('https://www.youlikehits.com/youtubenew2.php')
@@ -32,10 +29,6 @@ class Instagram:
             yt_view.click()
             sleep(130)
 
-    def close_browser(self):
-        Setup.close_browser(self)
-
-ig = Instagram()
-ig.setup()
-ig.go_to_website()
-ig.close_browser()
+ylh = YouLikeHits()
+ylh.setup()
+ylh.go_to_website()
