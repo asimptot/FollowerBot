13cb89eb12567b3c11e3bd1009e7b11757b06548
Asım <asimzorlu@gmail.com>
2022-11-11 13:07:20 +0100

    Update get_credit_on_YouLikeHits.py

--- get_credit_on_YouLikeHits.py
+++ get_credit_on_YouLikeHits.py
@@ -12,15 +12,15 @@ class YouLikeHits:
         self.browser.get('https://www.youlikehits.com/login.php')
         sleep(10)
         uid = self.browser.find_element(By.ID, 'username')
-        uid.send_keys('YOUR YOULIKEHITS USERNAME')
+        uid.send_keys(YOUR YOULIKEHITS USERNAME)
         sleep(2)
         pwd = self.browser.find_element(By.ID, 'password')
-        pwd.send_keys('YOUR YOULIKEHITS PASSWORD')
+        pwd.send_keys(YOUR YOULIKEHITS PASSWORD)
         sleep(2)
         btn = self.browser.find_element(By.XPATH, '/html/body/table[2]/tbody/tr/td/table/tbody/tr/td/table/tbody/tr[2]/td/center/form/table/tbody/tr[3]/td/span/input')
         btn.click()
         sleep(2)
-        
+
         print('Getting credits... Please do not terminate the program.')
         while (True):
             self.browser.get('https://www.youlikehits.com/youtubenew2.php')
@@ -30,8 +30,23 @@ class YouLikeHits:
                 yt_view.click()
                 sleep(130)
             except:
-                sleep(600)
-                print("\nAn error has been occurred. It will re-run again...")        
+                while (True):
+                    self.browser.get('https://www.youlikehits.com/websites.php')
+                    sleep(4)
+                    N = 15  # number of times you want to press TAB
+                    actions = ActionChains(self.browser)
+                    for _ in range(N):
+                        actions = actions.send_keys(Keys.TAB)
+                    actions.perform()
+                    sleep(2)
+                    actions.send_keys(Keys.ENTER)
+                    actions.perform()
+                    sleep(25)
+                    child = self.browser.window_handles[1]
+                    self.browser.switch_to.window(child)
+                    self.browser.close()
+                    parent = self.browser.window_handles[0]
+                    self.browser.switch_to.window(parent)
 
 ylh = YouLikeHits()
 ylh.setup()
