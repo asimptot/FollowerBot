d78f21bdf412d8637aab298cdb70cfaa064d854b
Asım <asimzorlu@gmail.com>
2022-11-13 09:29:32 +0100

    Update init.py

--- init.py
+++ init.py
@@ -4,6 +4,8 @@ import warnings
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 class Setup:
     def init(self):
