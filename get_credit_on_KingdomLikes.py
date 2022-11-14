b62db0f70b9f6ab062b76030643cf7da3fefce14
Asım <asimzorlu@gmail.com>
2022-11-14 18:59:45 +0100

    Update get_credit_on_KingdomLikes.py
    It will be run fully automated.

--- get_credit_on_KingdomLikes.py
+++ get_credit_on_KingdomLikes.py
@@ -2,9 +2,6 @@ from time import sleep
 import sys
 sys.path.append(r'C:\\Projects\\Get_Free_Followers')
 from init import *
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 
 class KingdomLikes:
     def setup(self):
@@ -13,18 +10,18 @@ class KingdomLikes:
     def go_to_website(self):
         sleep(4)
         self.browser.get('https://kingdomlikes.com/')
-        sleep(10)
+        sleep(5)
 
         uid = self.browser.find_element(By.NAME, 'email')
         uid.send_keys(YOUR KINGDOMLIKES EMAIL ADDRESS)
         sleep(2)
         pwd = self.browser.find_element(By.NAME, 'password')
-        pwd.send_keys(YOUR PASSWORD)
+        pwd.send_keys(YOUR KINGDOMLIKES PASSWORD)
         sleep(2)
         btn = self.browser.find_element(By.ID, 'buttonmobile')
         btn.click()
         sleep(2)
-        #You must add your social media account to YouLikeHits system.
+        #You must add your social media account to KingdomLikes system.
         print('Getting credits... Please do not terminate the program.')
         while (True):
             self.browser.get('https://kingdomlikes.com/free_points/youtube-views')
@@ -34,15 +31,24 @@ class KingdomLikes:
                 yt_view.click()
                 element_present = EC.presence_of_element_located(
                     (By.XPATH, '//*[@id="idpage6"]/div/div[5]/button'))
-                WebDriverWait(self.browser, 1000).until(element_present)
+                WebDriverWait(self.browser, 350).until(element_present)
             except:
-                sleep(1200)
-                print("\nAn error has been occurred. It will re-run again...")
+                while(True):
+                    self.browser.get('https://kingdomlikes.com/free_points/web-traffic')
+                    sleep(4)
+                    surf = self.browser.find_element(By.XPATH, '//*[@id="idpage5"]/div/div[5]/button')
+                    surf.click()
+                    element_present = EC.presence_of_element_located(
+                        (By.XPATH, '//*[@id="idpage6"]/div/div[5]/button'))
+                    WebDriverWait(self.browser, 350).until(element_present)
 
     def close_browser(self):
         Setup.close_browser(self)
 
 kl = KingdomLikes()
-kl.setup()
-kl.go_to_website()
-kl.close_browser()
+while(True):
+    kl.setup()
+    try:
+        kl.go_to_website()
+    except:
+        kl.close_browser()
