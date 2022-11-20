5a8b23bbd22813a6c653f1acc30c6386c7bea443
Asım <asimzorlu@gmail.com>
2022-11-21 00:56:54 +0100

    Update get_credit_on_AddMeFast.py

--- get_credit_on_AddMeFast.py
+++ get_credit_on_AddMeFast.py
@@ -7,8 +7,6 @@ from random import randint
 class AddMeFast:
     def setup(self):
         Setup.init(self)
-
-    def go_to_website(self):
         sleep(4)
         self.browser.get('https://addmefast.com')
         sleep(4)
@@ -23,14 +21,27 @@ class AddMeFast:
         button.click()
         sleep(6)
 
+    def surf_website(self):
         self.browser.get('https://addmefast.com/websites')
         while("No item" not in self.browser.page_source):
             sleep(25)
             point = self.browser.find_element(By.XPATH, '//*[@id="toppointsbalance"]')
             print('Your point is: ' + point.text)
-        delay = randint(1, 300)
-        print('We could not find a content. Searching for a new website to surf...')
-        sleep(delay)
+
+    def youtube_view(self):
+        self.browser.get('https://addmefast.com/free_points/youtube_views')
+        while("No item" not in self.browser.page_source or "orientate" not in self.browser.page_source):
+            sleep(4)
+            watch = WebDriverWait(self.browser, 10).until(
+                EC.presence_of_element_located((By.LINK_TEXT, "View Video"))
+            )
+            watch.click()
+            WebDriverWait(self.browser, 100).until(
+                EC.presence_of_element_located((By.XPATH, '//*[@id="content"]/div[1]/div[1]/div/strong[2]'))
+            )
+            point = self.browser.find_element(By.XPATH, '//*[@id="toppointsbalance"]')
+            print('Your point is: ' + point.text)
+            self.browser.refresh()
 
     def close_browser(self):
         Setup.close_browser(self)
@@ -38,8 +49,12 @@ class AddMeFast:
 amf = AddMeFast()
 
 while(True):
-    amf.setup()
     try:
-        amf.go_to_website()
+        amf.setup()
+        amf.surf_website()
+        amf.youtube_view()
     except:
+        delay = randint(1, 300)
+        print('We could not find a content to collect points. We are waiting ' + str(delay) + ' seconds...')
+        sleep(delay)
         amf.close_browser()
