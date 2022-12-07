5cd8fea81c107283343e7734f8ed2e227be57bfb
Asım <asimzorlu@gmail.com>
2022-12-07 22:04:28 +0100

    Update get_credit_on_KtechSeven.py
    Point info message has been added.

--- get_credit_on_KtechSeven.py
+++ get_credit_on_KtechSeven.py
@@ -6,8 +6,6 @@ from init import *
 class KtechSeven:
     def setup(self):
         Setup.init(self)
-
-    def go_to_website(self):
         sleep(4)
         self.browser.get('https://ktechseven.com/')
         sleep(5)
@@ -20,31 +18,36 @@ class KtechSeven:
         btn.click()
         sleep(2)
 
-        print('Getting credits... Please do not terminate the program.')
-        while(True):
-            for i in range(2):
-                self.browser.get('https://ktechseven.com/p.php?p=youtube')
-                sleep(4)
-                yt_view = self.browser.find_element(By.CLASS_NAME, 'followbutton')
-                yt_view.click()
-                sleep(2)
+    def get_point(self):
+        point = self.browser.find_element(By.ID, 'c_coins')
+        print('Your point is: ' + point.text)
+        return point
 
-            N = 26
-            play = ActionChains(self.browser)
-            for _ in range(N):
-                play.send_keys(Keys.TAB).perform()
+    def go_to_website(self):
+        print('Getting credits... Please do not terminate the program.')
+        for i in range(2):
+            self.browser.get('https://ktechseven.com/p.php?p=youtube')
+            sleep(4)
+            yt_view = self.browser.find_element(By.CLASS_NAME, 'followbutton')
+            yt_view.click()
             sleep(2)
-            play.send_keys(Keys.RETURN).perform()
-            sleep(70)
+        N = 26
+        play = ActionChains(self.browser)
+        for _ in range(N):
+            play.send_keys(Keys.TAB).perform()
+        sleep(2)
+        play.send_keys(Keys.RETURN).perform()
+        sleep(70)
 
     def close_browser(self):
         Setup.close_browser(self)
 
 ks = KtechSeven()
+ks.setup()
 while(True):
-    ks.setup()
     try:
         ks.go_to_website()
+        ks.get_point()
     except:
         print('Content not found to watch or surf. Refreshing webpage...')
         sleep(60)
