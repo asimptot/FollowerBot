18c09179705e0640d33d0196272977ae0d41bd05
Asım <asimzorlu@gmail.com>
2022-12-07 10:21:49 +0100

    Update get_credit_on_YouLikeHits.py
    Fixed https://github.com/asimptot/FollowerBot/issues/23#issuecomment-1340377916 issue.

--- get_credit_on_YouLikeHits.py
+++ get_credit_on_YouLikeHits.py
@@ -15,10 +15,9 @@ class YouLikeHits:
     def go_to_website(self):
         sleep(4)
         self.browser.get('https://www.youlikehits.com/login.php')
-        sleep(10)
+        sleep(5)
         uid = self.browser.find_element(By.ID, 'username')
         uid.send_keys(YOUR YOULIKEHITS USERNAME)
-        sleep(2)
         pwd = self.browser.find_element(By.ID, 'password')
         pwd.send_keys(YOUR YOULIKEHITS PASSWORD)
         sleep(2)
@@ -28,18 +27,15 @@ class YouLikeHits:
 
         print('Getting credits... Please do not terminate the program.')
         while(True):
-            self.browser.get('https://www.youlikehits.com/youtubenew2.php')
             try:
+                self.browser.get('https://www.youlikehits.com/youtubenew2.php')
                 sleep(4)
                 yt_view = self.browser.find_element(By.XPATH, '//*[@id="listall"]/center/a[1]')
                 yt_view.click()
 
-                while(True):
-                    element_present = EC.presence_of_element_located(
-                        (By.XPATH, "//*[contains(text(), 'Points Added')]"))
-                    WebDriverWait(self.browser, 1000).until(element_present)
-                    yt_view = self.browser.find_element(By.XPATH, '//*[@id="listall"]/center/a[1]')
-                    yt_view.click()
+                element_present = EC.presence_of_element_located(
+                    (By.XPATH, "//*[contains(text(), 'Points Added')]"))
+                WebDriverWait(self.browser, 1000).until(element_present)
 
             except:
                 while(True):
@@ -55,15 +51,16 @@ class YouLikeHits:
                     self.browser.close()
                     parent = self.browser.window_handles[0]
                     self.browser.switch_to.window(parent)
+                    sleep(2)
 
     def close_browser(self):
         Setup.close_browser(self)
 
 ylh = YouLikeHits()
-ylh.setup()
 while(True):
     try:
+        ylh.setup()
         ylh.go_to_website()
     except:
         ylh.get_point()
-        sleep(300)
+        print('Content not found to watch or surf. Refreshing webpage...')
