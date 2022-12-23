ad67600fe8b18de4162c1a426ca9480b66ad8e56
Asım <asimzorlu@gmail.com>
2022-12-23 11:20:58 +0100

    Update get_credit_on_AddMeFast.py
    Reddit Upvote Task timeout has been decreased from 19 to 18.5 seconds.

--- get_credit_on_AddMeFast.py
+++ get_credit_on_AddMeFast.py
@@ -753,7 +753,7 @@ class AddMeFast:
         except:
             sleep(1)
 
-        sleep(20)
+        sleep(18.5)
         self.browser.close()
         parent = self.browser.window_handles[0]
         self.browser.switch_to.window(parent)
