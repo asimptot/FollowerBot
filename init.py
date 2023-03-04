598fca252d78cddef6a88cdebe28639495bebce7
Asım <asimzorlu@gmail.com>
2023-03-04 10:26:48 +0100

    Update init.py
    undetected_chromedriver has been reactivated.

--- init.py
+++ init.py
@@ -1,26 +1,21 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
 import warnings
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
+from random import randint, sample, choice
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 import undetected_chromedriver as uc
 
 class Setup:
     def init(self):
-        warnings.filterwarnings("ignore")
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument('--window-size=1036, 674')
-        chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
-        chrome_options.add_experimental_option('useAutomationExtension', False)
-        chrome_options.add_argument('--headless')
-        chrome_options.add_argument("--log-level=3")
-        chrome_options.add_experimental_option("prefs", {
-            "profile.default_content_setting_values.notifications": 1
-        })
-        self.browser = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=chrome_options, )
+        chrome_options = uc.ChromeOptions()
+        chrome_options.add_argument('--headless=new')
+        self.browser = uc.Chrome(options=chrome_options, version_main=110)
 
     def close_browser(self):
         self.browser.close()
+        
