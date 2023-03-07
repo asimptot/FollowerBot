79cb5bcf37b039c1b98a03351dfa18a84e6ae2ef
Asım <asimzorlu@gmail.com>
2023-03-07 11:54:38 +0100

    Update init.py
    init.py file has been updated.

--- init.py
+++ init.py
@@ -8,14 +8,14 @@ from selenium.webdriver.common.by import By
 from random import randint, sample, choice
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.window import WindowTypes
 import undetected_chromedriver as uc
 
 class Setup:
     def init(self):
         chrome_options = uc.ChromeOptions()
-        chrome_options.add_argument('--headless=new')
+        chrome_options.add_argument("--headless=new")
         self.browser = uc.Chrome(options=chrome_options, version_main=110)
 
     def close_browser(self):
         self.browser.close()
-        
