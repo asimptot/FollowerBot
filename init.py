380f242d779e76feae5f1cfad5da070cbb259f6c
Asım <asimzorlu@gmail.com>
2023-03-19 21:04:19 +0100

    Update init.py
    Pop-up has been disabled.

--- init.py
+++ init.py
@@ -10,12 +10,19 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.window import WindowTypes
 import undetected_chromedriver as uc
+import sys
 
 class Setup:
     def init(self):
         chrome_options = uc.ChromeOptions()
         chrome_options.add_argument("--headless=new")
         chrome_options.add_argument("--mute-audio")
+        chrome_options.add_argument("--disable-popup-blocking")
+        chrome_options.add_argument("--disable-notifications")
+        chrome_options.add_argument('--disable-extensions')
+        chrome_options.add_argument('--disable-translate')
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--no-sandbox')
         prefs = {"credentials_enable_service": False,
                  "profile.password_manager_enabled": False}
         chrome_options.add_experimental_option("prefs", prefs)
